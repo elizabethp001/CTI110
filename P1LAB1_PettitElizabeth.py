c3ba95b9153f3591cb603d99afa1471c36fcfe7f
elizabethp001 <pettite8155@student.faytechcc.edu>
2025-02-23 09:52:42 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_PettitElizabeth.py
@@ -0,0 +1,8 @@
+# Elizabeth Pettit
+# 02/21/2024
+# P1Lab1
+# Teaching students how to create a Python Program using IDLE.
+
+first_name = input('Enter your first name')
+last_name = input('Enter your last name')
+print('Hello!', first_name, last_name, 'Welcome to the course CTI-110')
