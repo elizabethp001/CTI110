d91706fe42456b33d3c8466db4192145f5228c11
elizabethp001 <pettite8155@student.faytechcc.edu>
2025-03-01 23:43:50 -0500

    Add files via upload

---
+++ P1HW1_PettitElizabeth.py
@@ -0,0 +1,17 @@
+# Elizabeth Pettit
+# 03/01/2025
+# P1HW1
+# Use python's input and print functions.
+
+#----Calculating Exponents----
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+result = base * exponent
+print(base, "raised to the power of", exponent, "is", result, "!!")
+
+#----Addition and Subtration----
+num1 = int(input("Enter a starting integer: "))
+num2 = int(input("Enter an integer to add: "))
+num3 = int(input("Enter an integer to subtract: "))
+result = num1 + num2 - num3
+print(num1, "+", num2, "-", num3, "is equal to", result)
