388a26c9983b33a121d864ff6a057e285de5d9e7
elizabethp001 <pettite8155@student.faytechcc.edu>
2025-03-08 00:37:35 -0500

    Add files via upload

---
+++ P2Lab1_PettitElizabeth.py
@@ -0,0 +1,14 @@
+# Elizabeth Pettit
+# 03/07/2025
+# P2LAB1
+# Write code that performs mathematical calculations
+
+radius = float(input("Enter the radius of the circle: "))
+diameter = 2 * radius
+circumference = 2 * math.pi * radius
+area = math.pi * (radius ** 2)
+
+print('What is the radius of the circle?', radius)
+print('The diameter of the circle is', diameter)
+print('The circumference of the circle is', circumference)
+print('The area of the circle is', area)
